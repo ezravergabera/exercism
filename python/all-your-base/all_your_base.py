0a37713dd16a8910b075e27d86f29db6fd222260
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2025-08-26 02:41:44 +0800

    Solved All Your Base

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -2,23 +2,27 @@ def rebase(input_base, digits, output_base):
     if input_base < 2:
         raise ValueError("input base must be >= 2")
     
+    if any(d >= input_base for d in digits):
+        raise ValueError("all digits must satisfy 0 <= d < input base")
+        
+    if any(d < 0 for d in digits):
+        raise ValueError("all digits must satisfy 0 <= d < input base")
+    
     if output_base < 2:
         raise ValueError("output base must be >= 2")
-
-    number = str(digits)
-    total = 0
     
-    for index, num in enumerate(number[-1::-1]):
-        total += (int(num) * (input_base ** index))
+    total = 0
+    for index, num in enumerate(digits):
+        total += num * (input_base ** (len(digits)-1 - index))
 
-    res = ""
+    res = []
     while True:
-        res += str(total%output_base)
+        res.append(total%output_base)
         total = total//output_base
         if total == 1:
-            res += "1"
+            res.append(1)
+            break
+        elif total == 0:
             break
 
     return res[-1::-1]
-
-print(rebase(10, 42, 2))

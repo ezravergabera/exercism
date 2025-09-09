e85be3c1161533f4140dc277a5573488ac1f25c1
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2025-09-09 23:44:25 +0800

    Incomplete Solution to Tisbury Treasure Hunt

---
+++ python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,71 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    :param record: tuple - with a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
+    return record[1]
+
+
+def convert_coordinate(coordinate):
+    """Split the given coordinate into tuple containing its individual components.
+
+    :param coordinate: str - a string map coordinate
+    :return: tuple - the string coordinate split into its individual components.
+    """
+    return tuple(coordinate)
+
+
+def compare_records(azara_record, rui_record):
+    """Compare two record types and determine if their coordinates match.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
+    :return: bool - do the coordinates match?
+    """
+    return True if tuple(azara_record[1]) == rui_record[1] else False
+
+
+def create_record(azara_record, rui_record):
+    """Combine the two record types (if possible) and create a combined record group.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
+    """
+    if tuple(azara_record[1]) == rui_record[1]:
+        return azara_record + rui_record
+    else:
+        return "not a match"
+
+
+def clean_up(combined_record_group):
+    """Clean up a combined record group into a multi-line string of single records.
+
+    :param combined_record_group: tuple - everything from both participants.
+    :return: str - everything "cleaned", excess coordinates and information are removed.
+
+    The return statement should be a multi-lined string with items separated by newlines.
+
+    (see HINTS.md for an example).
+    """
+    clean_list = []
+    for record in combined_record_group:
+        clean_record = []
+        for index, data in enumerate(record):
+            if index == 1:
+                continue
+            if index == len(record)-1:
+                clean_record.append(data)
+                clean_list.append(tuple(clean_record))
+                continue
+            clean_record.append(data)
+
+    ans = 
+            
+    return ans
+    
+print(clean_up((('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'), ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'), ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'))))

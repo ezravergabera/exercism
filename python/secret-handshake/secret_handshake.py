948853ae7a819df7548d6f8de54e239bff480366
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2025-09-15 22:44:56 +0800

    Solved Secret Handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,19 @@
 def commands(binary_str):
-    pass
+    reverse = False
+    cmds = []
+    for index, bit in enumerate(binary_str):
+        if index == 0 and bit == "1":
+            reverse = True
+        if index == 1 and bit == "1":
+            cmds.insert(0, "jump")
+        if index == 2 and bit == "1":
+            cmds.insert(0, "close your eyes")
+        if index == 3 and bit == "1":
+            cmds.insert(0, "double blink")
+        if index == 4 and bit == "1":
+            cmds.insert(0, "wink")
+
+    if reverse:
+        return cmds[::-1]
+    else:
+        return cmds

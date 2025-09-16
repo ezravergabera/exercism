581c75417161246b5c6f28afb29726b79d860963
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2025-09-16 22:26:01 +0800

    Initial Solution to Anagram

---
+++ python/anagram/anagram.py
@@ -0,0 +1,10 @@
+def find_anagrams(word, candidates):
+    for candidate in candidates:
+        target = word
+        for letter in candidate:
+            if letter in target:
+                target
+
+stringgg = "hello"
+
+stringgg.index()
